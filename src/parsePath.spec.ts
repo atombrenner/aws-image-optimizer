859061c54b700bfd458a42369a291112a924503c
Christian Rodemeyer <atombrenner@gmail.com>
2022-12-01 14:09:58 +0100

    add minimal path parsing

--- src/parsePath.spec.ts
+++ src/parsePath.spec.ts
@@ -1,11 +1,49 @@
 import { parsePath } from './parsePath'
 
-describe('parseParams', () => {
+// all transformation information is encoded in the image path
+// Example: /path/to/image/45-a34fasd-4711/avif/400x300/10,20-5000,4000-1000,1000
+//          /$imagePath/$id/$format/$dimension/$crop
+
+describe('parsePath', () => {
   it.each(['/path/to/image/UUID', '/path/to/image/UUID/', '/path/to/image/UUID/400x300'])(
-    'should extract original key from path %p',
+    'should extract id from path %p',
     (path) => {
-      const params = parsePath(path)
-      expect(params?.originalKey).toEqual('path/to/image/UUID')
+      const { id } = parsePath(path)
+      expect(id).toEqual('UUID')
     }
   )
+
+  it('should not extract image id if path does not start with IMAGE_PATH', () => {
+    expect(parsePath('/wrongpath/UUID').id).toBeUndefined()
+  })
+
+  it('should not extract image id if path does contain an image id', () => {
+    expect(parsePath('/path/to/image/').id).toBeUndefined()
+  })
+
+  it.each([
+    ['avif', '/path/to/image/UUID/avif'],
+    ['webp', '/path/to/image/UUID/webp/'],
+    ['jpeg', '/path/to/image/UUID/jpeg/400x300'],
+  ])('should extract image type %p from from path %p', (expectedType, path) => {
+    const { type } = parsePath(path)
+    expect(type).toEqual(expectedType)
+  })
+
+  it.each(['/path/to/image/UUID/', 'path/to/imgage/UUID/png'])(
+    'should not extract image type if image type is not valid',
+    (path) => {
+      const { type } = parsePath(path)
+      expect(type).toBeUndefined()
+    }
+  )
+
+  it.each([
+    [{ width: 400, height: 300 }, '/path/to/image/UUID/avif/400x300'],
+    [{ height: 200 }, '/path/to/image/UUID/webp/x200'],
+    [{ width: 100 }, '/path/to/image/UUID/jpeg/100'],
+  ])('should extract dimensions %p from from path %p', (expectedDimension, path) => {
+    const { width, height } = parsePath(path)
+    expect({ width, height }).toEqual(expectedDimension)
+  })
 })
--- src/parsePath.ts
+++ src/parsePath.ts
@@ -1,35 +1,38 @@
 import { env } from './env'
+import { imageTypes, ProcessingParams } from './processImage'
 
 const imagePath = env('IMAGE_PATH')
-
 if (!imagePath.endsWith('/') || !imagePath.startsWith('/'))
   throw Error(`IMAGE_PATH "${imagePath}" must start and end with a trailing slash "/"`)
 
-export type Params = {
-  originalPath?: string
-  params?: {
-    type: 'webp' | 'jpeg' | 'avif'
-    width?: number
-    height?: number
-  }
-
-  // width or height or both
-  // cropArea
-  // focuspoint
-  // signature validation
+export type Params = ProcessingParams & {
+  id?: string
 }
 
 // parse params from path
-// example: /image/uuid/webp/300x400/342-344-434-34-234-234/signature
+// example: /path/to/image/uuid/webp/300x400/342-344-434-34-234-234
 
 export const parsePath = (path: string): Params => {
   if (!path.startsWith(imagePath)) return {}
+
   const segments = path.substring(imagePath.length).split('/')
-  const id = segments[0]
+  const id = segments[0] ? segments[0] : undefined
+  const type = imageTypes.includes(segments[1]) ? segments[1] : undefined
+  const { width, height } = parseDimensions(segments[2])
+
   return {
-    originalPath: imagePath + id,
-    params: {
-      type: 'avif',
-    },
+    id,
+    type,
+    width,
+    height,
+  }
+}
+
+const parseDimensions = (dimensions: string) => {
+  if (!dimensions) return {}
+  const [w, h] = dimensions.split('x')
+  return {
+    width: Number.parseInt(w) || undefined,
+    height: Number.parseInt(h) || undefined,
   }
 }
